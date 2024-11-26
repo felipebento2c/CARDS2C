e217effd6b94109b9e0e36a665545f8404d8733c
Aluno Felipe Bento <00001117281218sp@aluno.educacao.sp.gov.br>
2024-11-26 08:20:42 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,53 +1,53 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'CSS',
+    'O que é CSS?',
+    'É uma linguagem para estilizar páginas HTML'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'CSS',
+    'Para que serve o id no CSS',
+    'Estilizar um elemento único'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'CSS',
+    'Como mudar a cor de de fundo com CSS?',
+    'Usebackground-color'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'HTML',
+    'Para que serve a tag <a>?',
+    'Criar links'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'HTML',
+    'O que é uma tag <div>?',
+    'Um contêiner de bloco para agrupar elementos'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'HTML',
+    'Como criar uma lista numerada?',
+    'Use a tag<ol> com <li>'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'JavaScript',
+    'O que é JavaScript?',
+    'Linguagem para adicionar interatividade em paginas web'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'JavaScript',
+    'Como exibir uma menssagem de alerta?',
+    'Use alert()'
 )
 
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'JavaScript',
+    'Para que serve o let no JavaScript?',
+    'Declarar variaveis com escopo de bloco'
 )
